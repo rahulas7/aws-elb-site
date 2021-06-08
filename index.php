c75be1c6676912db043def92ac7db7f732c43bbf
rahulas7 <76993352+rahulas7@users.noreply.github.com>
2021-06-08 17:57:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ https://www.tooplate.com/view/2114-pixie
                <?php
 $output = shell_exec('echo $HOSTNAME');
 echo "<h1><center><pre>$output</pre></center></h1>";
-echo "<h1><center> Staging Myapp-v2 </center></h1>"
+echo "<h1><center> Development v1 </center></h1>"
 ?>
             </span>
           </div>
