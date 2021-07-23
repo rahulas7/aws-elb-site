16607f6cb129ffe890ed105ffe4e072510c3f447
Rahul AS <76993352+rahulas7@users.noreply.github.com>
2021-07-24 02:30:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ https://www.tooplate.com/view/2114-pixie
                <?php
 $output = shell_exec('echo $HOSTNAME');
 echo "<h1><center><pre>$output</pre></center></h1>";
-echo "<h1><center> Success Asg rolling Jenkins automated :) : ) </center></h1>"
+echo "<h1><center> Asg rolling Jenkins automated  </center></h1>"
 ?>
             </span>
           </div>
